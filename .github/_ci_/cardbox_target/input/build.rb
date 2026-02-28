03f03b906001b426a2bc7b00b960155db4fd34f5
Moe <m@tmoe.me>
2026-03-01 06:27:58 +0800

    chore(_ci_): json => jsonc

--- .github/_ci_/cardbox_target/input/build.rb
+++ .github/_ci_/cardbox_target/input/build.rb
@@ -5,6 +5,7 @@
 # ===================================
 # frozen_string_literal: true
 
+require 'yaml'
 require 'json'
 require 'cinnabar'
 using Cinnabar::Command::ArrRefin
@@ -13,11 +14,12 @@ workflow_yml = 'build_cardbox_target.yml'
 
 # json data
 stdin_data = File
-  .expand_path('build.json', __dir__)
-  .then { JSON.load_file _1 }
+  .expand_path('build.jsonc', __dir__)
+  .then { YAML.safe_load_file _1 }
   .except('$schema') # remove schema key
   .transform_values(&:to_s) # gh cli v2.87 requires JSON values to be Strings
   .then { JSON.dump _1 }
+  # .tap { warn _1 }
 
 %w[gh workflow run]
   .concat([workflow_yml, '--json'])
