d392b3275445ed83d5dbdacdb97051e515318860
Moe <m@tmoe.me>
2026-03-01 17:25:28 +0800

    chore(cardbox-target/build.rs): refactor build script

--- crates/cardbox-target/build.rs
+++ crates/cardbox-target/build.rs
@@ -9,36 +9,38 @@ fn main() {
   set_target_env().expect("Failed to update env")
 }
 
-fn update_rustc_link_cfg() {
-  // linux
-  if let Ok("linux") = cargo_cfg!(target_os).as_deref() {
-    "cargo:rustc-link-arg=-nostartfiles".puts();
-    "cargo:rustc-link-arg=-Wl,-e,_start".puts();
-    if let Ok("musl") = cargo_cfg!(target_env).as_deref() {
-      "cargo::rustc-link-lib=c".puts();
-    }
-  }
-  // if let Ok("loongarch64-unknown-linux-musl" | "mipsel-unknown-linux-musl") =
-  //   env::var("TARGET").as_deref()
-  // { "cargo::rustc-link-arg=-Wl,-z,max-page-size=0x1000".puts() }
+fn print_rs_link_arg(arg: &str) {
+  println!("cargo:rustc-link-arg={arg}")
+}
 
-  // macOS
-  if let Ok("macos") = cargo_cfg!(target_os).as_deref() {
-    "cargo::rustc-link-lib=System".puts()
+fn update_rustc_link_cfg() {
+  let Ok(os) = cargo_cfg!(target_os) else {
+    return;
+  };
+
+  match os.as_ref() {
+    "linux" => {
+      let _: [(); _] = ["-nostartfiles", "-Wl,-e,_start"].map(print_rs_link_arg);
+
+      if let Ok("musl") = cargo_cfg!(target_env).as_deref() {
+        print_rs_link_arg("c")
+      }
+    }
+
+    "macos" => print_rs_link_arg("System"),
+    _ => {}
   }
+
+  // if let Ok("loongarch64-unknown-linux-musl") =
+  //   env::var("TARGET").as_deref()
+  // { print_rs_link_arg("-Wl,-z,max-page-size=0x1000") }
 }
 
 fn set_target_env() -> io::Result<()> {
   let env_err =
     |e| io::Error::other(format!("Failed to get env var; VarError: {e}"));
+
   let target = env::var("TARGET").map_err(env_err)?;
   println!("cargo::rustc-env=__cardbox_cfg_target={target}");
   Ok(())
 }
-// ===========
-pub trait Puts: core::fmt::Display {
-  fn puts(&self) {
-    println!("{self}")
-  }
-}
-impl Puts for str {}
