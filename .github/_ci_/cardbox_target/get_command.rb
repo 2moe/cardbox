d11b6139f24a7d9e94a494c3a9989e8fbc875990
Moe <m@tmoe.me>
2026-03-01 16:08:49 +0800

    chore(cardbox_target): implement JSON data memoization for command loading

--- .github/_ci_/cardbox_target/get_command.rb
+++ .github/_ci_/cardbox_target/get_command.rb
@@ -1,11 +1,17 @@
 # Note: This is not a CLI binary, but a library that gets called, so no need to add a shebang.
 require 'json'
 
+# Decoded JSON Hash data memoization.
+class JsonData
+  def self.command
+    @command_file ||= File.expand_path('command.json', __dir__)
+    @command_hash ||= JSON.load_file(@command_file).freeze
+    @command_hash
+  end
+end
+
 def get_command(key, raise_err: true)
-  File
-    .expand_path('command.json', __dir__)
-    .then {|p| JSON.load_file p}
-    .then {|json| json[key]}
+  JsonData.command[key]
     .tap { raise "command_arr is nil" if _1.nil? && raise_err }
 end
 
